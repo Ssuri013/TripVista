acb9fc6e95fd909c11ca916f98b03f5c41362a35
Shubham Suri <shubham013@live.com>
2020-03-11 16:50:03 -0300

    angular routers

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { PlacesComponent } from './places/places.component';
+import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlacesComponent,
+    LoginComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'places', component: PlacesComponent },
+      { path: 'bookings', component: BookingComponent },
+      { path: 'login', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
