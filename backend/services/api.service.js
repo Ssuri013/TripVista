38626279f9dd30a53fe7d88eac393191318e0f3a
Shubham Suri <shubham013@live.com>
2020-03-14 13:28:08 -0300

    cleanup

--- backend/services/api.service.js
+++ backend/services/api.service.js
@@ -12,15 +12,11 @@ module.exports = {
 	name: "api",
 	mixins: [ApiGateway],
 
-	// More info about settings: https://moleculer.services/docs/0.14/moleculer-web.html
 	settings: {
-		// Exposed port
 		port: process.env.PORT || 3000,
 
-		// Exposed IP
 		ip: "0.0.0.0",
 
-		// Global Express middlewares. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Middlewares
 		use: [],
 		cors: {
 			origin: "**",
@@ -38,21 +34,16 @@ module.exports = {
 					"**"
 				],
 
-				// Route-level Express middlewares. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Middlewares
 				use: [],
 
-				// Enable/disable parameter merging method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Disable-merging
 				mergeParams: true,
 
 				// Enable authentication. Implement the logic into `authenticate` method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Authentication
 				authentication: false,
 
-				// Enable authorization. Implement the logic into `authorize` method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Authorization
 				authorization: false,
 
-				// The auto-alias feature allows you to declare your route alias directly in your services.
-				// The gateway will dynamically build the full routes from service schema.
-				autoAliases: true,
+				autoAliases: false,
 
 				aliases: {
 
@@ -83,7 +74,6 @@ module.exports = {
 					return doSomething(ctx, res, data);
 				}, */
 
-				// Calling options. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Calling-options
 				callingOptions: {},
 
 				bodyParsers: {
@@ -97,27 +87,19 @@ module.exports = {
 					}
 				},
 
-				// Mapping policy setting. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Mapping-policy
-				mappingPolicy: "all", // Available values: "all", "restrict"
+				mappingPolicy: "all",
 
-				// Enable/disable logging
 				logging: true
 			}
 		],
 
-		// Do not log client side errors (does not log an error response when the error.code is 400<=X<500)
 		log4XXResponses: false,
-		// Logging the request parameters. Set to any log level to enable it. E.g. "info"
 		logRequestParams: null,
-		// Logging the response data. Set to any log level to enable it. E.g. "info"
 		logResponseData: null,
 
-
-		// Serve assets from "public" folder. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Serve-static-files
 		assets: {
 			folder: "public",
 
-			// Options to `server-static` module
 			options: {}
 		}
 	},
@@ -137,25 +119,19 @@ module.exports = {
 		 * @returns {Promise}
 		 */
 		async authenticate(ctx, route, req) {
-			// Read the token from header
 			const auth = req.headers["authorization"];
 
 			if (auth && auth.startsWith("Bearer")) {
 				const token = auth.slice(7);
 
-				// Check the token. Tip: call a service which verify the token. E.g. `accounts.resolveToken`
 				if (token == "123456") {
-					// Returns the resolved user. It will be set to the `ctx.meta.user`
 					return { id: 1, name: "John Doe" };
 
 				} else {
-					// Invalid token
 					throw new ApiGateway.Errors.UnAuthorizedError(ApiGateway.Errors.ERR_INVALID_TOKEN);
 				}
 
 			} else {
-				// No token. Throw an error or do nothing if anonymous access is allowed.
-				// throw new E.UnAuthorizedError(E.ERR_NO_TOKEN);
 				return null;
 			}
 		},
@@ -171,10 +147,8 @@ module.exports = {
 		 * @returns {Promise}
 		 */
 		async authorize(ctx, route, req) {
-			// Get the authenticated user.
 			const user = ctx.meta.user;
 
-			// It check the `auth` property in action schema.
 			if (req.$action.auth == "required" && !user) {
 				throw new ApiGateway.Errors.UnAuthorizedError("NO_RIGHTS");
 			}
